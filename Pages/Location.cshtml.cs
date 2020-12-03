43854cc92ae2576643b1db063feaa814a3beb6d2
SithuAung <gyrosithu007@gmail.com>
2020-12-03 11:00:41 +0630

    Add project files.

---
+++ Pages/Location.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageMovie.Pages
+{
+    public class LocationModel : PageModel
+    {
+        private readonly ILogger<LocationModel> _logger;
+
+        public LocationModel(ILogger<LocationModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
